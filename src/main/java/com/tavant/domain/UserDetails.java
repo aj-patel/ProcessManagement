15a7c4227b00c3b2054214186cf3100d33ac54ad
AjayPatel <connecttoaj@gmail.com>
2014-11-19 03:20:57 +0530

    creating dao layer Fixes #2

---
+++ src/main/java/com/tavant/domain/UserDetails.java
@@ -0,0 +1,60 @@
+package com.tavant.domain;
+
+
+/**
+ * @author connecttoaj
+ *Domain class for userdetails.
+ */
+public class UserDetails {
+	
+	private int userId;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String password;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
