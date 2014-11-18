b671ae74ed8bf21265258685ded9f5c144b23a42
AjayPatel <connecttoaj@gmail.com>
2014-11-19 03:16:11 +0530

    creating domain objects Fixes #3

---
+++ src/main/java/com/tavant/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.tavant.dao;
+
+/**
+ * @author connecttoaj
+ *This is an interface for User related databse interaction.
+ */
+public interface UserDao {
+	
+	public boolean addUser(final UserDetails userDetails);
+
+}
