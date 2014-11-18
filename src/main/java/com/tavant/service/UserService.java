a0479ee0c8180508b36efe6e44e99903972a0d7f
AjayPatel <connecttoaj@gmail.com>
2014-11-19 03:29:06 +0530

    added service

---
+++ src/main/java/com/tavant/service/UserService.java
@@ -0,0 +1,9 @@
+package com.tavant.service;
+
+import com.tavant.domain.UserDetails;
+
+public interface UserService {
+	
+	public void addUserSerive(final UserDetails userDetails);
+
+}
---
+++ src/main/java/com/tavant/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.tavant.service.impl;
+
+import com.tavant.domain.UserDetails;
+import com.tavant.service.UserService;
+
+public class UserServiceImpl implements UserService{
+
+	public void addUserSerive(UserDetails userDetails) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
