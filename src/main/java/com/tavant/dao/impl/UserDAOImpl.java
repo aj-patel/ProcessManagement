cd1c3254faeb3f5536fa1cc3d3167b4c994cb5cd
aj-patel <connecttoaj@gmail.com>
2014-11-19 17:22:52 +0530

    added basic jdbc configuration

--- src/main/java/com/tavant/dao/impl/UserDAOImpl.java
+++ src/main/java/com/tavant/dao/impl/UserDAOImpl.java
@@ -1,10 +1,19 @@
 package com.tavant.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.tavant.dao.UserDao;
 import com.tavant.domain.UserDetails;
 
 public class UserDAOImpl implements UserDao{
-	
-	public boolean addUser(UserDetails userDetails) {return false;};
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	public boolean addUser(UserDetails userDetails) {
+		
+		
+		return false;
+	}
 
 }
