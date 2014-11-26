d463482dcd1a861a2dd5eecd838099e5719f9ca6
shakil248 <abdul.bengaluru@gmail.com>
2014-11-26 12:47:15 +0530

    sql queries added

---
+++ src/main/java/com/tavant/sql/SQLQueries.java
@@ -0,0 +1,44 @@
+package com.tavant.sql;
+
+
+import java.util.List;
+
+public interface SQLQueries {
+
+	public String getAddUserQuery();
+	
+	public String getUserDetailsQuery();
+	
+	public String getCreateTask();
+	
+	public String getTaskQuery(String steps);
+	
+	public String getCompleteTaskQuery();
+	
+	public String getRoleIdByUser();
+	
+	public String getStepsByRole();
+	
+	public String getTaskProgressByUser();
+	
+	public String getTask(String steps);
+	
+	public String createTaskProgressQuery();
+	
+	public String updateTaskQuery();
+	
+	public String getProcessInstanceQuery();
+	
+	public String getTaskCompleteQuery();
+	
+	public String getProcessCompleteQuery();
+	
+	public String getEndTaskQuery();
+	
+	public String getAllProcessListQuery();
+
+	public String getTaskIdsForRoleId();
+
+	public String getTaskListFromProcessInstance();
+
+}
