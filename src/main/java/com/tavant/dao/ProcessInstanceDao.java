cd6c4fc5c80109850b2c11df5d740f4153b7568b
Ajay Patel <connecttoaj@gmail.com>
2014-11-27 13:18:45 +0530

    updated

--- src/main/java/com/tavant/dao/ProcessInstanceDao.java
+++ src/main/java/com/tavant/dao/ProcessInstanceDao.java
@@ -7,7 +7,10 @@ import com.tavant.domain.ProcessInstanceDetails;
 
 public interface ProcessInstanceDao {
 	
-	public List<Integer> getTasksIdsFromProcessInst(List taskIds);
+	public List<Map> getTasksIdsFromProcessInst(List taskIds);
+	
+	public void updateProcessInstanceWithUserId(int processInstanceId,
+			int userId);
 	
 	public int insertApplicantDetails(ApplicantDetails applicantDetails);
 	
